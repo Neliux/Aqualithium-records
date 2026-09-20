237d392f2e5e3a5bd623f9b7f7eed4b951be6523
Neliux <alonsofede.dante@gmail.com>
2026-09-20 19:43:29 -0300

    Actualizar caché de PWA tras reparaciones visuales

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='swim-quest-final-20260920-09';
+const CACHE='swim-quest-final-20260920-10';
 const ASSETS=[
   './',
   './index.html',
@@ -16,7 +16,7 @@ function patchGame(text){
   if(!out.includes('src="./final-fixes.js')){
     out=out.replace(
       '</body>',
-      '<script src="./workout-clan-fix.js?v=20260917-1"></script><script id="swq-sw-final-fixes" src="./final-fixes.js?v=20260920-09"></script></body>'
+      '<script src="./workout-clan-fix.js?v=20260917-1"></script><script id="swq-sw-final-fixes" src="./final-fixes.js?v=20260920-10"></script></body>'
     );
   }
   return out;
