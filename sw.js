7e5d932de1874d9f8b7950146250a81006d5953d
Neliux <alonsofede.dante@gmail.com>
2026-09-22 20:48:08 -0300

    Bump Swim Quest service worker cache version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='swim-quest-final-20260922-20';
+const CACHE='swim-quest-final-20260922-21';
 const ASSETS=[
   './',
   './index.html',
@@ -16,7 +16,7 @@ function patchGame(text){
   if(!out.includes('src="./final-fixes.js')){
     out=out.replace(
       '</body>',
-      '<script src="./workout-clan-fix.js?v=20260917-1"></script><script id="swq-sw-final-fixes" src="./final-fixes.js?v=20260922-20"></script></body>'
+      '<script src="./workout-clan-fix.js?v=20260917-1"></script><script id="swq-sw-final-fixes" src="./final-fixes.js?v=20260922-21"></script></body>'
     );
   }
   return out;
