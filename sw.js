1bdf7cbd1b7a95f490f8401d1326d8d3432a81c2
Neliux <alonsofede.dante@gmail.com>
2026-09-23 13:45:07 -0300

    Bump service worker cache for style UI repair

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='swim-quest-final-20260922-30';
+const CACHE='swim-quest-final-20260923-31';
 const ASSETS=[
   './',
   './index.html',
@@ -16,7 +16,7 @@ function patchGame(text){
   if(!out.includes('src="./final-fixes.js')){
     out=out.replace(
       '</body>',
-      '<script src="./workout-clan-fix.js?v=20260917-1"></script><script id="swq-sw-final-fixes" src="./final-fixes.js?v=20260922-30"></script></body>'
+      '<script src="./workout-clan-fix.js?v=20260917-1"></script><script id="swq-sw-final-fixes" src="./final-fixes.js?v=20260923-31"></script></body>'
     );
   }
   return out;
